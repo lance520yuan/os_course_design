4ca1df9eaee45f1013c5b3257014d8be557f27de
lanceYuan <18645369158@163.com>
2020-06-03 22:00:41 +0800

    new:新增主页

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- encoding: utf-8 -*- 
+""" 
+@File    :   main    
+@Contact :   18645369158@163.com  
+@Modify Time      @Author    @Version    @Desciption 
+------------      -------    --------    ----------- 
+2020/6/3 21:50   LanceYuan  1.0         None 
+"""
